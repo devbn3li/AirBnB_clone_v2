494b7c47888386b18b47240522ca6d89959b1840
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-12-18 14:31:00 +0200

    a Bash script that sets up web servers for the deployment of web_static.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,9 +2,11 @@
 # Set up web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
 
 # Create a fake HTML file for testing
 sudo echo "<html>
@@ -16,10 +18,13 @@ sudo echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Create or recreate the symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
+# Give ownership of the /data/ folder to the ubuntu user AND group
 sudo chown -R ubuntu:ubuntu /data/
 
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
+# Restart nginx
 sudo service nginx restart
