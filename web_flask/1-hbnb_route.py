65642e9ef4ba00435d9fb05a32aa0cf2350800b0
devbn3li <fudwhc@gmail.com>
2023-12-23 16:25:00 +0200

    push

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,17 +5,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """Return hello hbnb"""
 
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Return hbnb"""
 
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
