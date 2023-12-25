9f186c4d9ed7c96b846e00540eaa5ac80be1acfa
devbn3li <fudwhc@gmail.com>
2023-12-25 16:53:57 +0200

    pu

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -3,41 +3,40 @@
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """Return hello hbnb"""
+    """Return a given string"""
+    return ("Hello HBNB!")
 
-    return 'Hello HBNB!'
 
-
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Return hbnb"""
-
-    return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """Return C followed by text"""
-
-    return 'C %s' % text.replace('_', ' ')
+    """Returns a given string"""
+    return ("HBNB")
 
 
-@app.route('/python/<text>', strict_slashes=False)
-def python(text):
-    """Return Python followed by text"""
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    """display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
-    return 'Python %s' % text.replace('_', ' ')
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """Return n is a number if n is an integer"""
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonText(text="is cool"):
+    """display Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
 
-    return '%d is a number' % n
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/number/<int:n>", strict_slashes=False)
+def isNumber(n):
+    """display “n is a number” only if n is an integer"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -2,50 +2,60 @@
 """Start a Flask web application"""
 
 from flask import Flask, render_template
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """Return hello hbnb"""
+    """Return a given string"""
+    return "Hello HBNB!"
 
-    return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Return hbnb"""
+    """Returns a given string"""
+    return "HBNB"
 
-    return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """Return C followed by text"""
+def cText(text):
+    """display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
-    return 'C %s' % text.replace('_', ' ')
 
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
-    """Return Python followed by text"""
+def pythonText(text="is cool"):
+    """display Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
 
-    return 'Python %s' % text.replace('_', ' ')
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """Return n is a number if n is an integer"""
+def isNumber(n):
+    """display “n is a number” only if n is an integer"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
 
-    return '%d is a number' % n
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """Return HTML page if n is an integer"""
+def number_template(n=None):
+    """display a HTML page only if n is an integer"""
+    if isinstance(n, int):
+        return render_template("5-number.html", n=n)
 
-    return render_template('5-number.html', n=n)
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    """Return HTML page if n is an integer"""
+def number_odd_or_even(n=None):
+    """display a HTML page only if n is an integer:
+    H1 tag: “Number: n is even|odd” inside the tag BODY
+    """
+    if isinstance(n, int):
+        if n % 2:
+            eo = "odd"
+        else:
+            eo = "even"
+        return render_template("6-number_odd_or_even.html", n=n, eo=eo)
 
-    return render_template('6-number_odd_or_even.html', n=n)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run()
