98696fb7c226c093e52fb111a9bf0e456a93f14f
devbn3li <fudwhc@gmail.com>
2023-12-25 16:43:48 +0200

    up

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -3,37 +3,33 @@
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """Return hello hbnb"""
+    """Return a given string"""
+    return ("Hello HBNB!")
 
-    return 'Hello HBNB!'
 
-
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Return hbnb"""
-
-    return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """Return C followed by text"""
-
-    return 'C %s' % text.replace('_', ' ')
+    """Returns a given string"""
+    return ("HBNB")
 
 
-@app.route('/python/<text>', strict_slashes=False)
-@app.route('/python/', strict_slashes=False)
-def python(text):
-    """Return Python followed by text"""
-
-    return 'Python %s' % text.replace('_', ' ')
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    """display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonText(text="is cool"):
+    """display Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
