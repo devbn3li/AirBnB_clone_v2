b81db2fae994caea9fea277b98d31a6554eac70a
devbn3li <fudwhc@gmail.com>
2023-12-26 12:28:15 +0200

    update

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-""" """
+"""test for state"""
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
-import os
 
 
 class test_state(test_basemodel):
-    """ states test class"""
+    """this will test the state class"""
 
     def __init__(self, *args, **kwargs):
-        """ state test class init"""
+        """ """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ testing state name attr"""
+        """ """
         new = self.value()
-        self.assertEqual(type(new.name), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+        self.assertEqual(type(new.name), str)
