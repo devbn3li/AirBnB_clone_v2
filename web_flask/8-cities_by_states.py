757e13d84ab08f4dd9460cb12447cf35bb2a088b
devbn3li <fudwhc@gmail.com>
2023-12-26 12:40:41 +0200

    aaaaaaaaaaaaaaaaaah

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,33 +4,23 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.user import User
-from models.review import Review
 
 
 app = Flask(__name__)
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Displays the main HBNB page"""
-    states = storage.all(State).values()
-    amenities = storage.all(Amenity).values()
-    places = storage.all(Place).values()
-    users = storage.all(User).values()
-    reviews = storage.all(Review).values()
-    return render_template('100-hbnb.html', states=states, amenities=amenities,
-                           places=places, users=users, reviews=reviews)
-
-
 @app.teardown_appcontext
-def teardown(self):
-    """Method to remove SQLAlchemy Session"""
+def close(self):
+    """ Method to close the session """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays a html page with states and cities"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
