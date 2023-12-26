10891e078a680356f86fe6eb016db7fdb15dfe67
devbn3li <fudwhc@gmail.com>
2023-12-26 12:07:58 +0200

    lol

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,51 +2,25 @@
 """Start a Flask web application"""
 
 from flask import Flask, render_template
+from models import storage
+from models.state import State
+
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello_hbnb():
-    """Display text"""
-    return 'Hello HBNB!'
-@app.route('/hbnb')
-def hbnb():
-    """Display text"""
-    return 'HBNB'
-@app.route('/c/<text>')
-def c(text):
-    """Display text"""
-    return 'C %s' % text.replace('_', ' ')
-@app.route('/python')
-@app.route('/python/<text>')
-def python(text='is cool'):
-    """Display text"""
-    return 'Python %s' % text.replace('_', ' ')
-@app.route('/number/<int:n>')
-def number(n):
-    """Display text"""
-    return '%d is a number' % n
-@app.route('/number_template/<int:n>')
-def number_template(n):
-    """Display text"""
-    return render_template('5-number.html', n=n)
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """Render state_list html page to display States created"""
+    states = storage.all()
+    return render_template('7-states_list.html', states=states)
 
-@app.route('/number_odd_or_even/<int:n>')
-def number_odd_or_even(n):
-    """Display text"""
-    return render_template('6-number_odd_or_even.html', n=n)
-
-@app.route('/states_list')
-def states_list():
-    """Display text"""
-    return render_template('7-states_list.html')
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Close storage"""
+def teardown(self):
+    """Method to remove current SQLAlchemy Session"""
     storage.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
