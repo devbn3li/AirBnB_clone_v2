9051c9a6f32cf658fe5bd96b2251a51872a18525
devbn3li <fudwhc@gmail.com>
2023-12-27 23:31:12 +0200

    up

--- models/state.py
+++ models/state.py
@@ -22,14 +22,14 @@ class State(BaseModel, Base):
     @property
     def cities(self):
         var = models.storage.all()
-        lista = []
+        listaa = []
         result = []
         for key in var:
             city = key.replace('.', ' ')
             city = shlex.split(city)
             if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
+                listaa.append(var[key])
+        for elem in listaa:
             if (elem.state_id == self.id):
                 result.append(elem)
         return (result)
