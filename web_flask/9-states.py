02cd43cfb15cb3dd93c30fbb770be2c3394b12f4
devbn3li <fudwhc@gmail.com>
2023-12-28 00:08:20 +0200

    uuu

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,27 +9,27 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def home():
-    """A function that serves as the home route of the application.
-    Returns:
-        str: The greeting message "Hello HBNB!".
-    """
-    return "Hello HBNB!"
-
-
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Displays a html page with states and cities"""
-    states = storage.all(State)
-    return render_template("7-states_list.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown_context(self):
-    """Closes the storage session"""
+def close(self):
+    """ Method to close the session """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/states', strict_slashes=False)
+def state():
+    """Displays a html page with states"""
+    states = storage.all(State)
+    return render_template('9-states.html', states=states, mode='all')
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_by_id(id):
+    """Displays a html page with citys of that state"""
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', states=state, mode='id')
+    return render_template('9-states.html', states=state, mode='none')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
